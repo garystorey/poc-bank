178c8e1703de18524c481e606a033d870ef1b868
Gary Storey <garystorey655@gmail.com>
2026-01-13 11:40:25 -0500

    updates

--- src/app/features/onboarding/pages/confirmation/confirmation.component.ts
+++ src/app/features/onboarding/pages/confirmation/confirmation.component.ts
@@ -1,11 +1,12 @@
 import { RouterLink } from "@angular/router";
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ButtonComponent } from '@shared/index';
+import { StepperComponent } from "../../components/stepper/stepper.component";
 
 @Component({
   selector: 'app-confirmation',
   standalone: true,
-  imports: [ButtonComponent, RouterLink],
+  imports: [ButtonComponent, RouterLink, StepperComponent],
   templateUrl: './confirmation.component.html',
   styleUrl: './confirmation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
